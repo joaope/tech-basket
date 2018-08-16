b90c76626c35d345cc644e076b6aceab87f91d34
Joao Correia <joaope@gmail.com>
2018-08-16 23:00:10 +0100

    added more offer tests

---
+++ tests/TechBasket.DomainService.UnitTests/TwoButtersGetBreadHalfPriceOfferTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using TechBasket.DomainService.Models;
+using Xunit;
+
+namespace TechBasket.DomainService.UnitTests
+{
+    public sealed class TwoButtersGetBreadHalfPriceOfferTests
+    {
+        private readonly IOffer _offer = new TwoButtersGetBreadHalfPriceOffer();
+
+        [Fact]
+        public void ApplyDiscount_TwoButters_ApplyDiscountToOneBread()
+        {
+            var pricedProducts = new[]
+            {
+                new PricedProduct(ProductIdentifier.Butter, 0.8m),
+                new PricedProduct(ProductIdentifier.Butter, 0.8m),
+                new PricedProduct(ProductIdentifier.Bread, 1m),
+                new PricedProduct(ProductIdentifier.Bread, 1m)
+            };
+
+            _offer.Apply(pricedProducts);
+
+            Assert.Equal(4, pricedProducts.Length);
+            Assert.True(pricedProducts.All(p => p.Identifier == ProductIdentifier.Bread || p.Identifier == ProductIdentifier.Butter));
+            Assert.Equal(1, pricedProducts.Count(p => p.Identifier == ProductIdentifier.Bread && p.DiscountedPrice == 0.5m));
+            Assert.Equal(1, pricedProducts.Count(p => p.Identifier == ProductIdentifier.Bread && p.DiscountedPrice == 1));
+            Assert.Equal(2, pricedProducts.Count(p => p.Identifier == ProductIdentifier.Butter && p.DiscountedPrice == 0.8m));
+        }
+    }
+}
