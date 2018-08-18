05879569d9124bf13854b6fe5c7acc7f5230bf1d
Joao Correia <joaope@gmail.com>
2018-08-18 15:28:46 +0100

    refactor tests to easily extend

--- tests/TechBasket.Web.UnitTests/ApiControllerTests.cs
+++ tests/TechBasket.Web.UnitTests/ApiControllerTests.cs
@@ -8,20 +8,27 @@ namespace TechBasket.Web.UnitTests
 {
     public class ApiControllerTests
     {
-        [Fact]
-        public void Test1()
+        private readonly Mock<IProductService> _productServiceMock;
+        private readonly ApiController _apiController;
+
+        public ApiControllerTests()
         {
-            var productServiceMock = new Mock<IProductService>();
-            productServiceMock
+            _productServiceMock = new Mock<IProductService>();
+            _apiController = new ApiController(_productServiceMock.Object);
+        }
+
+        [Fact]
+        public void ApiController_GetProducts_ReturnProductsFromDomainService()
+        {
+            _productServiceMock
                 .Setup(s => s.GetProducts())
                 .Returns(new[]
                 {
                     new Product("Milk", ProductIdentifier.Milk, 12m),
                     new Product("Butter", ProductIdentifier.Butter, 0.5m)
                 });
-            var apiController = new ApiController(productServiceMock.Object);
 
-            var products = apiController.GetProducts();
+            var products = _apiController.GetProducts();
 
             Assert.Equal(2, products.Count);
             Assert.Equal("Milk", products[0].Name);
@@ -30,6 +37,18 @@ namespace TechBasket.Web.UnitTests
             Assert.Equal("Butter", products[1].Name);
             Assert.Equal("Butter", products[1].Identifier);
             Assert.Equal(0.5m, products[1].Price);
+        }
+
+        [Fact]
+        public void ApiController_GetProducts_ReturnsEmptyProducts()
+        {
+            _productServiceMock
+                .Setup(s => s.GetProducts())
+                .Returns(() => null);
+
+            var products = _apiController.GetProducts();
+
+            Assert.Empty(products);
         }
     }
 }
